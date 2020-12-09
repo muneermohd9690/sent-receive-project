ae407250e15be07b4b7380c68eaece9cdccd6daa
muneermohd9690 <52971960+muneermohd9690@users.noreply.github.com>
2020-12-09 23:20:42 +0400

    issue voucher pdf
    pdf with logo for issue voucher.

--- sent_receive_project/forms/urls.py
+++ sent_receive_project/forms/urls.py
@@ -4,6 +4,8 @@ from .import views
 urlpatterns=[
     path('',views.forms),
     path('sent_items_invoice/',views.sent_items_invoice,name='sent_items_invoice'),
-    path('issue_vouchers/',views.issue_vouchers,name='issue_vouchers')
+    path('sent_items_invoice/print_sent_items_invoice',views.print_sent_items_invoice,name='print_sent_items_invoice'),
+    path('issue_vouchers/',views.issue_vouchers,name='issue_vouchers'),
+    path('issue_vouchers/print_issue_vouchers',views.print_issue_vouchers,name='print_issue_vouchers')
 
 ]
--- sent_receive_project/forms/views.py
+++ sent_receive_project/forms/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from io import BytesIO
+from xhtml2pdf import pisa
+from django.template.loader import render_to_string, get_template
+import os
+from django.conf import settings
+from django.contrib.staticfiles import finders
+
 
 def forms(request):
     return HttpResponse ("this is the forms page")
@@ -11,5 +18,61 @@ def sent_items_invoice(request):
 def issue_vouchers(request):
     return render(request,'issue_vouchers.html')
 
-def prosecution_printouts(request):
-    return render(request,'prosecution_printouts.html')
+
+def link_callback(uri, rel):
+    """
+    Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+    resources
+    """
+    result = finders.find(uri)
+    if result:
+        if not isinstance(result, (list, tuple)):
+            result = [result]
+        result = list(os.path.realpath(path) for path in result)
+        path = result[0]
+    else:
+        sUrl = settings.STATIC_URL  # Typically /static/
+        sRoot = settings.STATIC_ROOT  # Typically /home/userX/project_static/
+        mUrl = settings.MEDIA_URL  # Typically /media/
+        mRoot = settings.MEDIA_ROOT  # Typically /home/userX/project_static/media/
+
+        if uri.startswith(mUrl):
+            path = os.path.join(mRoot, uri.replace(mUrl, ""))
+        elif uri.startswith(sUrl):
+            path = os.path.join(sRoot, uri.replace(sUrl, ""))
+        else:
+            return uri
+
+    # make sure that file exists
+    if not os.path.isfile(path):
+        raise Exception(
+            'media URI must start with %s or %s' % (sUrl, mUrl)
+        )
+    return path
+
+def print_sent_items_invoice(request):
+
+    data = {}
+    template = get_template("print_sent_items_invoice.html")
+    data_p = template.render(data)
+    response = BytesIO()
+    #pisa.pisaDocument(StringIO.StringIO(html.encode("UTF-8")), dest=result, link_callback=links)
+
+    pdfPage = pisa.pisaDocument(BytesIO(data_p.encode("UTF-8")), response)
+    if not pdfPage.err:
+        return HttpResponse(response.getvalue(), content_type="application/pdf")
+    else:
+        return HttpResponse("Error")
+
+def print_issue_vouchers(request):
+
+    data = {}
+    template = get_template("print_issue_vouchers.html")
+    data_p = template.render(data)
+    response = BytesIO()
+
+    pdfPage = pisa.pisaDocument(BytesIO(data_p.encode("UTF-8")), response)
+    if not pdfPage.err:
+        return HttpResponse(response.getvalue(), content_type="application/pdf")
+    else:
+        return HttpResponse("Error")
