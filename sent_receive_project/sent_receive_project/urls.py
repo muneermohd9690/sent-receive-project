9b8e55789699d500d40ff8025a0a3a6467700936
muneermohd9690 <52971960+muneermohd9690@users.noreply.github.com>
2021-10-08 14:36:12 +0400

    added static files
    added lines for static files in urls.py

--- sent_receive_project/sent_receive_project/urls.py
+++ sent_receive_project/sent_receive_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.views.static import serve
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,4 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
