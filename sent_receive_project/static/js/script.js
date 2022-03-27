d5eb736830b91e931c86f6ec673c75a0c3d1274b
muneermohd9690 <52971960+muneermohd9690@users.noreply.github.com>
2022-03-28 00:17:44 +0400

    delete confirmation
    delete confirmation added using bootstrap for items and toners app

--- sent_receive_project/static/js/script.js
+++ sent_receive_project/static/js/script.js
@@ -39,58 +39,58 @@ $(document).ready(function(){
 });
 /**** edit_tonerdetails_form ****/
 
-/**** disable save button in edit_tonerdetails_form ****/
+/**** disable save button in edit_tonerdetails_form(ETDF) ****/
 $(document).ready(function(){
     $("#employee_name,#employee_name2").on("keyup",function(){
-        $(".btn-action2").prop("disabled",false);
+        $(".btn-action-ETDF").prop("disabled",false);
             if(($("#employee_name").val())==($("#employee_name2").val())){
-                    $(".btn-action2").prop("disabled",true);
+                    $(".btn-action-ETDF").prop("disabled",true);
         }
     });
    });
 $(document).ready(function(){
     $("#employee_designation,#employee_designation2").on("keyup",function(){
-        $(".btn-action2").prop("disabled",false);
+        $(".btn-action-ETDF").prop("disabled",false);
             if(($("#employee_designation").val())==($("#employee_designation2").val())){
-                    $(".btn-action2").prop("disabled",true);
+                    $(".btn-action-ETDF").prop("disabled",true);
         }
     });
    });
 $(document).ready(function(){
-    $(".btn-action2").prop("disabled", true);
+    $(".btn-action-ETDF").prop("disabled", true);
     $("#toner_model, #toner_model2").change(function() {
         if( ($("#toner_model").val()) == ($("#toner_model2").val()) ) {
-            $(".btn-action2").attr("disabled", "disabled");
+            $(".btn-action-ETDF").attr("disabled", "disabled");
         }
         else {
-            $(".btn-action2").removeAttr("disabled");
+            $(".btn-action-ETDF").removeAttr("disabled");
         }
      });
    });
 $(document).ready(function(){
-    $(".btn-action2").prop("disabled", true);
+    $(".btn-action-ETDF").prop("disabled", true);
     $("#issued_to, #issued_to2").change(function() {
         if( ($("#issued_to").val()) == ($("#issued_to2").val()) ) {
-            $(".btn-action2").attr("disabled", "disabled");
+            $(".btn-action-ETDF").attr("disabled", "disabled");
         }
         else {
-            $(".btn-action2").removeAttr("disabled");
+            $(".btn-action-ETDF").removeAttr("disabled");
         }
      });
    });
  $(document).ready(function(){
-    $(".btn-action2").prop("disabled", true);
+    $(".btn-action-ETDF").prop("disabled", true);
     $("#status, #status2").change(function() {
         if( ($("#status").val()) == ($("#status2").val()) ) {
-            $(".btn-action2").attr("disabled", "disabled");
+            $(".btn-action-ETDF").attr("disabled", "disabled");
         }
         else {
-            $(".btn-action2").removeAttr("disabled");
+            $(".btn-action-ETDF").removeAttr("disabled");
         }
      });
    });
 
-/**** disable save button in edit_tonerdetails_form ****/
+/**** disable save button in edit_tonerdetails_form(ETDF) ****/
 
 /**** modal in edit_items_form ****/
 $(document).ready(function(){
@@ -135,3 +135,125 @@ $(document).ready(function(){
     });
 });
 /**** modal in edit_item_details_form ***/
+
+/**** disable save button in edit_toners_form(ETF) ****/
+$(document).ready(function(){
+    $("#toner_model,#toner_model2").on("keyup",function(){
+        $(".btn-action-ETF").prop("disabled",false);
+            if(($("#toner_model").val())==($("#toner_model2").val())){
+                    $(".btn-action-ETF").prop("disabled",true);
+        }
+    });
+   });
+
+ $(document).ready(function(){
+    $(".btn-action-ETF").prop("disabled", true);
+    $("#toner_printer, #toner_printer2").change(function() {
+        if( ($("#toner_printer").val()) == ($("#toner_printer2").val()) ) {
+            $(".btn-action-ETF").attr("disabled", "disabled");
+        }
+        else {
+            $(".btn-action-ETF").removeAttr("disabled");
+        }
+     });
+   });
+
+ /**** disable save button in edit_toners_form(ETF)  ****/
+
+ /**** disable save button in edit_items_form(EIF) ****/
+$(document).ready(function(){
+    $("#model_no,#model_no2").on("keyup",function(){
+        $(".btn-action-EIF").prop("disabled",false);
+            if(($("#model_no").val())==($("#model_no2").val())){
+                    $(".btn-action-EIF").prop("disabled",true);
+        }
+    });
+   });
+
+$(document).ready(function(){
+    $("#description,#description2").on("keyup",function(){
+        $(".btn-action-EIF").prop("disabled",false);
+            if(($("#description").val())==($("#description2").val())){
+                    $(".btn-action-EIF").prop("disabled",true);
+        }
+    });
+   });
+
+ /**** disable save button in edit_items_form(EIF) ****/
+
+/**** disable save button in edit_item_details_form(EIDF) ****/
+
+ $(document).ready(function(){
+    $(".btn-action-EIDF").prop("disabled", true);
+    $("#model_no, #model_no2").change(function() {
+        if( ($("#model_no").val()) == ($("#model_no2").val()) ) {
+            $(".btn-action-EIDF").attr("disabled", "disabled");
+        }
+        else {
+            $(".btn-action-EIDF").removeAttr("disabled");
+        }
+     });
+   });
+
+   $(document).ready(function(){
+    $("#serial_no,#serial_no2").on("keyup",function(){
+        $(".btn-action-EIDF").prop("disabled",false);
+            if(($("#serial_no").val())==($("#serial_no2").val())){
+                    $(".btn-action-EIDF").prop("disabled",true);
+        }
+    });
+   });
+
+
+   $(document).ready(function(){
+    $("#tag_no,#tag_no2").on("keyup",function(){
+        $(".btn-action-EIDF").prop("disabled",false);
+            if(($("#tag_no").val())==($("#tag_no2").val())){
+                    $(".btn-action-EIDF").prop("disabled",true);
+        }
+    });
+   });
+
+ $(document).ready(function(){
+    $(".btn-action-EIDF").prop("disabled", true);
+    $("#issued_to, #issued_to2").change(function() {
+        if( ($("#issued_to").val()) == ($("#issued_to2").val()) ) {
+            $(".btn-action-EIDF").attr("disabled", "disabled");
+        }
+        else {
+            $(".btn-action-EIDF").removeAttr("disabled");
+        }
+     });
+   });
+
+  $(document).ready(function(){
+    $("#employee_name,#employee_name2").on("keyup",function(){
+        $(".btn-action-EIDF").prop("disabled",false);
+            if(($("#employee_name").val())==($("#employee_name2").val())){
+                    $(".btn-action-EIDF").prop("disabled",true);
+        }
+    });
+   });
+
+   $(document).ready(function(){
+    $("#employee_designation,#employee_designation2").on("keyup",function(){
+        $(".btn-action-EIDF").prop("disabled",false);
+            if(($("#employee_designation").val())==($("#employee_designation2").val())){
+                    $(".btn-action-EIDF").prop("disabled",true);
+        }
+    });
+   });
+
+ $(document).ready(function(){
+    $(".btn-action-EIDF").prop("disabled", true);
+    $("#status, #status2").change(function() {
+        if( ($("#status").val()) == ($("#status2").val()) ) {
+            $(".btn-action-EIDF").attr("disabled", "disabled");
+        }
+        else {
+            $(".btn-action-EIDF").removeAttr("disabled");
+        }
+     });
+   });
+
+/**** disable save button in edit_item_details_form(EIDF) ****/
